2b73ff3b11259713a905a0aa5b6e3940e2a333fa
oluwatoni <ooogunma@edu.uwaterloo.ca>
2019-03-02 14:08:59 -0500

    add state machine to arm script

--- arm_scripts/press_play.py
+++ arm_scripts/press_play.py
@@ -2,105 +2,131 @@
 import threading
 import RPi.GPIO as GPIO
 import serial
+import crcmod
+import signal
+import sys
 
-baud_rate = 9600
-ser_AX = serial.Serial("/dev/ttyACM0", baud_rate)
-ser_XL = serial.Serial("/dev/ttyACM0", baud_rate)
+class MotorInterface(object):
+    def __init__(self, recording_freq, playback_freq):
+        self._states = ["waiting", "recording", "playback"]
+        self._current_state = 0
+        self._motor_angles = []
+        self._recording_index = 0
+        self._playback_index = 0
+        self._crc32 = crcmod.Crc(0x104c11db7, initCrc=0, xorOut=0xFFFFFFFF)
+        self._recording_freq = recording_freq
+        self._playback_freq = playback_freq
+        baud_rate = 9600
+        self._ser_AX = serial.Serial("/dev/ttyACM0", baud_rate)
+        #self._ser_XL = serial.Serial("/dev/ttyACM0", baud_rate)
 
-#TODO Remove these?
-global states = ["waiting", "recording", "playback"]
-global current_state = 0
-global motor_angles = []
-recording_index = 0
-playback_index = 0
-crc32 = crcmod.Crc(0x104c11db7, initCrc=0, xorOut=0xFFFFFFFF)
+    def signal_handler(self, sig, frame):
+        print('You pressed Ctrl+C!')
+        self._ser_AX.close()
+        sys.exit(0)
 
-def start_recording(channel):
-    if state == 2:
-        display("now overwriting the previous recording!")
-    state = 1
-    recording_index = 0
-    motor_angles = []
-    threading.Timer(1/recording_freq, recording(recording_freq)).start()
-  
-def start_playback(channel):  
-    if state == and len(motor_angles) != 0:
-        state = 2
-    elif len(motor_angles) == 0:
-        display("Nothing to be played back!")
-        return
-    playback_index = 0
-    threading.Timer(1/playback_freq, playing(playback_freq)).start()
+    def start_recording(self, channel):
+        if self._current_state != 1:
+            print "now overwriting the previous recording!"
+            self._current_state = 1
+            self._recording_index = 0
+            self._motor_angles = []
+            threading.Timer(1.0/self._recording_freq, self.recording()).start()
+ 
+    def start_playback(self, channel):  
+        if self._current_state != 2:
+            if len(motor_angles) == 0:
+                self._current_state = 0
+                print "Nothing to be played back!"
+                return
+            self._current_state = 2
+            self._playback_index = self._record_index
+            threading.Timer(1.0/self._playback_freq, self.playing()).start()
 
-def read_serial_port(ser):
-    ser.open()
-    n = ser.inWaiting()
-    response  = ""
-    for num in range(n):
-        response += ser.read()
-    ser.close()
-    response = response.strip("\r\n")
-    response = response.split(",")
-    checksum = response[-1]
-    response = response[0:-1]
-    crc32.update(",".join(map(str, response)))
+    def read_serial_port(self, ser):
+        response  = ""
+        if ser.inWaiting():
+            n = ser.inWaiting()
+            for num in range(n):
+                response += ser.read()
+            print response
+            response = response.strip("\r\n")
+            response = response.split(",")
+            checksum = response[-1]
+            response = response[0:-1]
+            self._crc32.update(",".join(map(str, response)))
 
-    if checksum != crc32.hexdigest()
-        display("Checksum mismatch!")
-        display("received: " + checksum)
-        display("calculated: " + crc32.hexdigest())
-        return NONE
-    return response
+            #if checksum != str(self._crc32.crcValue):
+                #print "Checksum mismatch!"
+                #print "received: " + checksum
+                #print "calculated: " + str(self._crc32.crcValue)
+                #return None
+            return response
+        else:
+            print "No data!"
 
-def write_to_serial_port(data, ser):
-    #Send a message to request the motor positions
-    # START_BIT, MODE, CHECKSUM
-    msg = "s,"
-    msg += ",".join(data)
-    crc32.update(msg)
-    msg += ","
-    msg += crc32.hexdigest()
-    msg += "\n"
-    ser.write(msg)
+    def write_to_serial_port(self, data, ser):
+        #Send a message to request the motor positions
+        # START_BIT, MODE, CHECKSUM
+        msg = "s,"
+        msg += ",".join(data)
+        msg += ","
+        self._crc32.update(msg)
+        msg += str(self._crc32.crcValue)
+        msg += "\n"
+        ser.write(msg)
 
-def recording(recording_freq):
-    msg = "" #TODO please define this
-    write_to_serial_port(msg, ser_AX)
-    write_to_serial_port(msg, ser_XL)
-    #Get a response from the AX controllers
-    AX_resp = read_serial_port(ser_AX)
-    XL_resp = read_serial_port(ser_XL)
+    def recording(self):
+        msg = ["2"]
+        self.write_to_serial_port(msg, self._ser_AX)
+        #self.write_to_serial_port(msg, self._ser_XL)
 
-    if AX_resp != NONE and XL_resp != NONE:
-        motor_angles[recording_index] = AX_resp[1:4]
-        motor_angles[recording_index] += XL_resp[1]
-        recording_index += 1
-    if state == 1:
-        threading.Timer(1/recording_freq, recording(recording_freq)).start()
+        #Get a response from the AX controllers
+        AX_resp = self.read_serial_port(self._ser_AX)
+        #XL_resp = self.read_serial_port(self._ser_XL)
 
+        if AX_resp != None: # and XL_resp != None:
+            if len(AX_resp) == 3:
+                self._motor_angles.append(AX_resp[1:4])
+                print self._motor_angles
+                #motor_angles[self._recording_index] += XL_resp[1]
+                self._recording_index += 1
+        if self._current_state == 1:
+            threading.Timer(1.0/recording_freq, self.recording()).start()
 
-def playing(playback_freq):
-    if (playback_index == len(motor_angles)):
-        state = 0
-    msg = "" #TODO please define this
-    write_to_serial_port(msg, ser_AX)
-    write_to_serial_port(msg, ser_XL)
-    if state == 2:
-        threading.Timer(1/playback_freq, playing(playback_freq)).start() 
+    def playing(self):
+        if (self._playback_index == 0):
+            self._current_state = 0
+        AX_msg = ["1"]
+        AX_msg += motor_angles[self._playback_index]
+        self.playback_index -= 1;
+        self.write_to_serial_port(AX_msg, self._ser_AX)
+        #self.write_to_serial_port(msg, self._ser_XL)
 
+        if self._current_state == 2:
+            threading.Timer(1.0/playback_freq, self.playing()).start() 
 
 if __name__ == '__main__':
-    recording_freq = 40
+    recording_freq = 80.0
     playback_freq = recording_freq
-      
+    arm = MotorInterface(recording_freq, playback_freq)
+
+    GPIO.setmode(GPIO.BCM)
+    # GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.  
+    # Both ports are wired to connect to GND on button press.  
+    # So we'll be setting up falling edge detection for both  
+    GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     # when a falling edge is detected on port 17, regardless of whatever   
     # else is happening in the program, the function my_callback will be run  
-    GPIO.add_event_detect(17, GPIO.FALLING, callback=start_recording, bouncetime=300)  
-    
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=arm.start_recording, bouncetime=300)  
+ 
     # when a falling edge is detected on port 23, regardless of whatever   
     # else is happening in the program, the function my_callback2 will be run  
     # 'bouncetime=300' includes the bounce control written into interrupts2a.py  
-    GPIO.add_event_detect(22, GPIO.FALLING, callback=start_playback, bouncetime=300)
+    GPIO.add_event_detect(22, GPIO.FALLING, callback=arm.start_playback, bouncetime=300)
+    signal.signal(signal.SIGINT, arm.signal_handler)
 
-    while true:
+    print("Starting the loop")
+    while True:
         pass
